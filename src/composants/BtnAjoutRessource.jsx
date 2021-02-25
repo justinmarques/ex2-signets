11bb7b226ede4dbb83c83b3bdc25e62ea11e85d2
justin <justinmarqus@gmail.com>
2021-02-25 16:31:08 -0500

    dernière intégration avant git hub pages

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -1,9 +1,13 @@
 import './BtnAjoutRessource.scss';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 export default function BtnAjoutRessource() {
   return (
     <button className="BtnAjoutRessource">
-      Ajouter
+      <Fab style={{backgroundColor:' #333', color:'#ffff'}}>
+        <AddIcon />
+      </Fab>
     </button>
   );
 }
--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -4,16 +4,16 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 export default function Dossier({id, titre, couleur, dateModif}) {
   return (
-    <article className="Dossier">
+    <article className="Dossier" style={{backgroundColor:couleur}}>
       <div className="couverture">
-        <span className="deplacer"><SortIcon /></span>
+        <span className="deplacer"><SortIcon style={{color:'#ffffff'}}/></span>
         <img src={`images-dossiers/${id}.png`} alt={titre}/>
       </div>
-      <div className="info" style={{backgroundColor:couleur}}>
+      <div className="info">
         <h2>{titre}</h2>
         <p>{dateModif}</p>
+        <button className="BtnMore"><MoreVertIcon style={{color:'#ffffff'}}/></button>
       </div>
-      <MoreVertIcon className="BtnMore"/>
     </article>
   );
 }
